f61087f8d21296cf6dc7d8e48fbc942a476ad043
Ricky Tanudjaja <rtanudjaja@student.unimelb.edu.au>
2017-03-29 00:04:33 +1100

    Merge my implementation with what Con did previously

--- legalMoves.java
+++ legalMoves.java
@@ -3,8 +3,7 @@ package projectA;
 import java.util.Scanner;
 
 public class legalMoves {
-
-	@SuppressWarnings("resource")
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -13,31 +12,43 @@ public class legalMoves {
 		// horizontal[]
 		
 		// important things to return
-		int numLegalHMoves = 0;
-		int numLegalVMoves = 0;
+		int numLegalHMoves = 0, numLegalVMoves = 0;
 		
 		// scanner + record piece location
+		@SuppressWarnings("resource")
 		Scanner boardScan = new Scanner(System.in);
 		// assume the input is correct
 		Board gameBoard = new Board(boardScan.nextInt());
 		
-		//System.out.println(gameBoard.getN());
+		// run a for loop to enter the data into the board
+		int x,y, vIndex = 0, hIndex = 0;
+				
+		//Maximum number of pieces per player is n, the dimension of the board
+		vPiece vertical[] = new vPiece[gameBoard.getN()];
+		hPiece horizontal[] = new hPiece[gameBoard.getN()];
 		
-		int y,x;
 		for(y = gameBoard.getN() - 1;y >= 0; y--) {
 			for(x = 0; x < gameBoard.getN(); x++) {
 				char readChar = boardScan.next().charAt(0);
 				gameBoard.enter(readChar, y, x);
-				//System.out.println("read for " + y + "," + x + " is " + readChar);
+				
+				//store pieces in easy access array
+				if(gameBoard.getChar(x, y) == 'V'){
+					vertical[vIndex] = new vPiece(x, y);
+					vIndex++;
+				}
+				else if(gameBoard.getChar(x, y) == 'H'){
+					horizontal[hIndex] = new hPiece(x, y);
+					hIndex++;
+				}
 			}
 		}
-		
-		//System.out.println("End scanning");
 		
 		// run a while/for loop to enter the data into the board
 		// identify pieces
 		// and record location at the same time
 		
+		//SIMPLE IMPLEMENTATION
 		for(y = gameBoard.getN() - 1;y >= 0; y--) {
 			for(x = 0; x < gameBoard.getN(); x++) {
 				if(gameBoard.getChar(x, y) == 'H') {
@@ -63,9 +74,9 @@ public class legalMoves {
 		// if move(1,0) run counter
 		// e.g. move(1,0,board) either returns true if possible else false
 		
-		
 	}
 	
+	// THIS FUNCTION IS REQUIRED FOR SIMPLE IMPLEMENTATION BUT I WOULD LIKE TO CHANGE THIS LATER
 	public static int checkMoves(piece thePiece, Board gameBoard) {
 		int counter = 0;
 		if(thePiece.move(1, 0, gameBoard)) {
