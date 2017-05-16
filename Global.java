1f44fdb68a4b52206336c5778c2a805c32727d74
Ricky Tanudjaja <rtanudjaja@student.unimelb.edu.au>
2017-05-16 10:46:44 +1000

    add H_Moves and V_MOVES

--- Global.java
+++ Global.java
@@ -1,10 +1,15 @@
 package CKRTsliderbot;
 
+import aiproj.slider.Move;
+import aiproj.slider.Move.Direction;
+
 public class Global {
 	
 	public static final char BLANK = '+';
 	public static final char BLOCK = 'B';
 	public static final char H_CELL = 'H';
 	public static final char V_CELL = 'V';
+	public static final Direction[] H_MOVES = {Move.Direction.RIGHT, Move.Direction.DOWN, Move.Direction.UP};
+	public static final Direction[] V_MOVES = {Move.Direction.UP, Move.Direction.LEFT, Move.Direction.RIGHT};
 
 }
